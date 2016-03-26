6886badc0cff8386f945610454d4b85c0268faee
Fabiano Nalin <fnalin@msn.com>
2016-03-26 20:23:49 -0300

    Demo Views - Jade

---
+++ 02. Server/Criando Views/UsandoJadeView/server.js
@@ -0,0 +1,18 @@
+﻿var http = require("http");
+var express = require("express");
+var app = express();
+
+//Configurando View Engine
+app.set("view engine", "jade");
+
+app.get("/", function (req, res) {
+	res.render("index", {title:"Express + Jade"});
+});
+
+app.get("/api/users", function (req, res) {
+	res.set("Content-Type", "application/json");
+	res.send({ nome: "Fabiano Nalin", ehValido: true, grupo: "Administrador" });
+});
+
+var server = http.createServer(app);
+server.listen(4000);
