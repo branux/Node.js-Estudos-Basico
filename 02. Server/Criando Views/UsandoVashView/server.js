95ec78ff1c865ce1a15d04af550da52480776a81
Fabiano Nalin <fnalin@msn.com>
2016-03-27 11:41:39 -0300

    Views - Vash

---
+++ 02. Server/Criando Views/UsandoVashView/server.js
@@ -0,0 +1,18 @@
+﻿var http = require("http");
+var express = require("express");
+var app = express();
+
+//Configurando View Engine
+app.set("view engine", "vash");
+
+app.get("/", function (req, res) {
+	res.render("index", { title: "Express + Vash" });
+});
+
+app.get("/api/users", function (req, res) {
+	res.set("Content-Type", "application/json");
+	res.send({ nome: "Fabiano Nalin", ehValido: true, grupo: "Administrador" });
+});
+
+var server = http.createServer(app);
+server.listen(4000);
