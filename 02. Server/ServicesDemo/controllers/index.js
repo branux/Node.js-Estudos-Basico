a35a57bfca63e919991429cad5c290749f287a44
Fabiano Alberto Nalin <fnalin@msn.com>
2016-03-29 11:15:36 -0300

    Services

---
+++ 02. Server/ServicesDemo/controllers/index.js
@@ -0,0 +1,11 @@
+﻿(function (controllers) {
+    
+    var homeController = require("./homeController.js");
+    var usersController = require("./usersController.js");
+    
+    controllers.init = function (app) {
+        app.set("view engine", "vash");
+        homeController.init(app);
+        usersController.init(app);
+    };
+}(module.exports));
---
+++ 02. Server/ServicesDemo/controllers/usersController.js
@@ -0,0 +1,8 @@
+﻿(function (usersController) {
+    usersController.init = function (app) {
+        app.get("/api/users", function (req, res) {
+            res.set("Content-Type", "application/json");
+            res.send({ nome: "Fabiano Nalin", ehValido: true, grupo: "Administrador" });
+        });
+    };
+}(module.exports));
